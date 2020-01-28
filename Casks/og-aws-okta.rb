acf9ae3504a0e3661c028bb8c2508a96358df8f7
Gary Schulte <gary.schulte@opengov.com>
2020-01-27 20:20:56 -0800

    fixed target

--- Casks/og-aws-okta.rb
+++ Casks/og-aws-okta.rb
@@ -6,5 +6,5 @@ cask 'og-aws-okta' do
   name 'og-aws-okta'
   homepage 'https://github.com/garyschulteog/homebrew-og'
 
-  artifact 'aws-okta', target: '/usr/local/bin/aws-okta'
+  artifact 'bin/aws-okta', target: '/usr/local/bin/aws-okta'
 end
