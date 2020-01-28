579a33401d1ebf03c991d1aea7a51cf022a6bea4
Gary Schulte <gary.schulte@opengov.com>
2020-01-27 17:02:23 -0800

    attempt at an og-aws-okta cask

---
+++ Casks/og-aws-okta.rb
@@ -0,0 +1,9 @@
+cask 'og-aws-okta' do
+  version '0.27.0'
+  sha256 '4b1b0a9dc9cb4efa621a5589cc63047193eda46c18c6a1150a22595f9a2c5bfc'
+
+  url 'https://github.com/garyschulteog/homebrew-og/raw/master/bin/og-aws-okta.tar.gz'
+  name '4Peaks'
+  homepage 'https://github.com/garyschulteog/homebrew-og'
+
+end
